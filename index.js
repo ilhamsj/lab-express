3b36b9feb81b28aa4477fb5a178584770cdc74e4
Ilham Saputrajati <ilhamsaputrajati@gmail.com>
2021-06-10 23:16:14 +0700

    simple port

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
-const app   = express();
-const port  = 3001;
+const app = express();
+const port = 3001;
 
 app.get("/", (request, response) => {
-    response.send();
+    response.send("test");
+})
+
+app.listen(port, () => {
+    console.log(`running on port http://localhost:${port}`)
 })
